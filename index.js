e0b2d9946752ee1d457b06aa4f69e5f31409da4c
Abdullah Omer Aidah <a.o.aidah.note9@gmail.com>
2022-12-25 04:03:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+//Code your solutions in this file
+function writeCards(stringName, eventName) {
+    let mes = [];
+    for (let i = 0; i < stringName.length; i++) {
+        mes.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return mes;
+}
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number)
+        number--
+    }
+    return number;
+}
+
